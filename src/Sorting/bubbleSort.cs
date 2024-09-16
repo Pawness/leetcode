1d0a8a5e83c36c6f03dfbf586a79e8a4ab28dcd8
Sai Maharshi Chinthalapalli <csaimaharshi@gmail.com>
2024-09-16 18:49:28 -0400

    Added some leetcode problems

---
+++ src/Sorting/bubbleSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace leetcode.src.Sorting
+{
+    public class bubbleSort : RunThisCode
+    {
+        public void bubbleSortAlgorithm()
+        {
+//            In Bubble Sort algorithm,
+
+//traverse from left and compare adjacent elements and the higher one is placed at right side. 
+//In this way, the largest element is moved to the rightmost end at first.
+//This process is then continued to find the second largest and place it and so on until the data is sorted.
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for(int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            Solution.printAnArray(arr);
+        }
+
+        public override void RunTheCode()
+        {
+            bubbleSortAlgorithm();
+        }
+    }
+}
---
+++ src/Sorting/quickSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.src.Sorting
+{
+    public class quickSort : RunThisCode
+    {
+        // have to do again
+        public int[] SortArray(int[] array, int leftIndex, int rightIndex)
+        {
+            int i = leftIndex;
+            int j = rightIndex - 1;
+
+            int pivot = array[leftIndex];
+
+            while(i <= j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+                if(i <= j)
+                {
+
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (leftIndex < j)
+                SortArray(array, leftIndex, j);
+            if (i < rightIndex)
+                SortArray(array, i, rightIndex);
+
+            Solution.printAnArray(array);
+            return array;
+        }
+        public override void RunTheCode()
+        {
+            SortArray(new int[] { 52, 96, 67, 71, 42, 38, 39, 40, 14 } , 0 , new int[] { 52, 96, 67, 71, 42, 38, 39, 40, 14 }.Length);
+        }
+    }
+}
